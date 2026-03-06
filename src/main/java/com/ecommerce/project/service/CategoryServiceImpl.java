4ef4e251bd313818a793ce02eefce7497530d351
Dhananjay Bhagat <dhananjaybhagat123@gmail.com>
2026-03-06 14:08:34 +0530

    ECOM-001: Add Category entity, repository, service, controller and basic CRUD APIs

---
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+        categoryRepository.delete(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully !!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Category updatedCategory = categoryRepository.findById(categoryId)
+                .stream()
+                .map(category1 -> {
+                    category1.setCategoryName(category.getCategoryName());
+                    categoryRepository.save(category1);
+                    return category1;
+                }).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+        return updatedCategory;
+    }
+}
