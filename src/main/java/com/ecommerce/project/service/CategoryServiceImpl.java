dcaa2f49328dd6a2007568e002662e72ee3bc6c4
Dhananjay Bhagat <dhananjaybhagat123@gmail.com>
2026-03-06 20:03:01 +0530

    ECOM-002: Add Validations and Handle Custom Exceptions using Global Exception Handler for Category flow

---
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,58 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) {
+            throw new APIException("No category created till now.");
+        }
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (savedCategory != null) {
+            throw new APIException("Category with the name: " + category.getCategoryName() + " already exists !!!");
+        }
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        categoryRepository.delete(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully !!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Category updatedCategory = categoryRepository.findById(categoryId)
+                .stream()
+                .map(category1 -> {
+                    category1.setCategoryName(category.getCategoryName());
+                    categoryRepository.save(category1);
+                    return category1;
+                }).findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        return updatedCategory;
+    }
+}
