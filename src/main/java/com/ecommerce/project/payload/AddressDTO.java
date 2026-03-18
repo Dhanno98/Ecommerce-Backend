47e35fc26ebf4b2f3ba783b09d1f091ae42fb583
Dhananjay Bhagat <dhananjaybhagat123@gmail.com>
2026-03-18 13:23:25 +0530

    ECOM-011 Add and manage user Address to ship orders

--- src/main/java/com/ecommerce/project/payload/AddressDTO.java
+++ src/main/java/com/ecommerce/project/payload/AddressDTO.java
@@ -1,25 +1,15 @@
-package com.ecommerce.project.model;
+package com.ecommerce.project.payload;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Table(name = "addresses")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Address {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class AddressDTO {
     private Long addressId;
 
     @NotBlank
@@ -45,17 +35,4 @@ public class Address {
     @NotBlank
     @Size(min = 6, message = "Pincode name must be at least 6 characters")
     private String pincode;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    public Address(String street, String buildingName, String city, String state, String country, String pincode) {
-        this.street = street;
-        this.buildingName = buildingName;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-        this.pincode = pincode;
-    }
 }
