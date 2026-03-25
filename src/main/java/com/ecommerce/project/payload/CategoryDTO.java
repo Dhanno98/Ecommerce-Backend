b8a51f40e06c3a039d89d63b6376ae155814e704
Dhananjay Bhagat <dhananjaybhagat123@gmail.com>
2026-03-25 13:03:39 +0530

    ECOM-015: Add API Documentation with Swagger and Spring Boot

--- src/main/java/com/ecommerce/project/payload/CategoryDTO.java
+++ src/main/java/com/ecommerce/project/payload/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
+
+    @Schema(description = "Category ID", example = "1")
     private Long categoryId;
 
+    @Schema(description = "Category name for category you wish to create", example = "Laptop")
     @NotBlank
     @Size(min = 5, message = "Category name must contain at least 5 characters")
     private String categoryName;
--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -43,6 +43,15 @@ public class JwtUtils {
         }
     }
 
+    // Get JWT Token from Header
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
     // Generate JWT Cookie from JWT Token
     public ResponseCookie generateJwtCookie(UserDetailsImpl userDetails) {
         String jwtToken = generateTokenFromUsername(userDetails.getUsername());
